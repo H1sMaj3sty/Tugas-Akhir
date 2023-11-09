cd6cdfc0a004e2823e579c3d85a2c064b8690a1c
Parjiman <ibnufarij032@gmail.com>
2023-11-09 19:31:18 +0700

    Commit Ibnu. Penambahan fungsi tombol button pada activity_about_page.xml untuk menuju tujuannya

--- app/src/main/java/com/example/tugasakhir/aboutPage.java
+++ app/src/main/java/com/example/tugasakhir/aboutPage.java
@@ -2,7 +2,10 @@ package com.example.tugasakhir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class aboutPage extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class aboutPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
+    }
+
+    public void faceBook(View view) {
+        String fbUrl = "https://www.facebook.com/";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(fbUrl));
+        startActivity(intent);
+    }
+
+    public void instaGram(View view) {
+        String igUrl = "https://www.instagram.com/";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(igUrl));
+        startActivity(intent);
+    }
+
+    public void youTube(View view) {
+        String ytUrl = "https://www.youtube.com/";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(ytUrl));
+        startActivity(intent);
     }
 }
