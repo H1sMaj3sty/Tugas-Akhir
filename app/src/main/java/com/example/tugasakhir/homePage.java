ea515d427c40562593d8a3ab5bcf26744d80e16f
Da Man <akuncadangannn2020@gmail.com>
2023-11-09 07:32:00 +0700

    Commit Danis, Perubahan AndroidManifest menjadi activty_main sebagai activy pertama yang muncul. Penambahan Intent pada MainActiviy untuk pindah ke SecondActivity

--- app/src/main/java/com/example/tugasakhir/homePage.java
+++ app/src/main/java/com/example/tugasakhir/homePage.java
@@ -3,6 +3,7 @@ package com.example.tugasakhir;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class homePage extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class homePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void padarIsland(View view) {
+
+    }
+
+    public void moveToDesc(String Title, String Desc, int ResourceImage) {
+
     }
 }
