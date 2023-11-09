34547514cb2d23ba7f77abe449fa23aeb9c66f55
Da Man <akuncadangannn2020@gmail.com>
2023-11-09 11:18:41 +0700

    Commit Danis, Penambahan activity_about.xml dan beberapa resources di drawable

--- app/src/main/java/com/example/tugasakhir/MainActivity.java
+++ app/src/main/java/com/example/tugasakhir/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
