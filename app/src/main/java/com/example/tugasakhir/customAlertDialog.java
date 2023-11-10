d4bdbca469f2070a95b9a6cd95ea4c8a80b87e16
berlymhrn <maharanyyberliana@gmail.com>
2023-11-10 10:57:13 +0700

    comit berli - alert ui

---
+++ app/src/main/java/com/example/tugasakhir/customAlertDialog.java
@@ -0,0 +1,14 @@
+package com.example.tugasakhir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customAlertDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_alert_dialog);
+    }
+}
